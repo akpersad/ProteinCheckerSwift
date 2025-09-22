240961b5f65b70d479f0250a8ef3c277fbb54cda
Andrew Persad <akpersad@gmail.com>
2025-09-21 21:44:53 -0400

    Initial Commit

---
+++ ProteinCheckerBuildable/ProteinCheckerBuildableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProteinCheckerBuildableApp.swift
+//  ProteinCheckerBuildable
+//
+//  Created by Persad, Andrew on 9/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ProteinCheckerBuildableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
